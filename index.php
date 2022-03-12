0747a9209c2840e6a6b0205eddf6da4cc3685944
Marie <Mari400m@edu.zealand.dk>
2022-03-12 17:45:16 +0100

    hej

--- index.php
+++ index.php
@@ -9,14 +9,17 @@
     <meta name="author" content="Udgiver">
     <meta name="copyright" content="Information om copyright">
 
-    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
-    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <link href="css/bootstrap.scss" rel="stylesheet" type="text/css">
+    <link href="css/styles.scss" rel="stylesheet" type="text/css">
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
 <body>
 
+<div>
+    <h1>Hej</h1>
+</div>
 
 
 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
