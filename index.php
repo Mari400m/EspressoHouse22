bb7bac38d9bebb6d0eaade318325729f1a3dfde7
Marie <Mari400m@edu.zealand.dk>
2022-03-13 11:27:29 +0100

    Vertical menu slettet

--- index.php
+++ index.php
@@ -17,13 +17,6 @@
 
 <body>
 
-<div class="vertical-menu">
-    <a href="#" class="active">Home</a>
-    <a href="#">Link 1</a>
-    <a href="#">Link 2</a>
-    <a href="#">Link 3</a>
-    <a href="#">Link 4</a>
-</div>
 
 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
