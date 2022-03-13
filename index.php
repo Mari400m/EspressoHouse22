4a8aeeaea449a89d2d3268556fbccbceb8011dce
Marie <Mari400m@edu.zealand.dk>
2022-03-13 12:24:34 +0100

    slideshow

--- index.php
+++ index.php
@@ -26,7 +26,65 @@
     <a href="#about">About</a>
 </div>
 
+<h2 style="text-align:center">Multiple Slideshows</h2>
 
+<p>Slideshow 1:</p>
+<div class="slideshow-container">
+    <div class="mySlides1">
+        <img src="images/1.png" style="width:100%">
+    </div>
+
+    <div class="mySlides1">
+        <img src="images/2.png" style="width:100%">
+    </div>
+
+    <div class="mySlides1">
+        <img src="images/12.png" style="width:100%">
+    </div>
+
+    <a class="prev" onclick="plusSlides(-1, 0)">&#10094;</a>
+    <a class="next" onclick="plusSlides(1, 0)">&#10095;</a>
+</div>
+
+<p>Slideshow 2:</p>
+<div class="slideshow-container">
+    <div class="mySlides2">
+        <img src="images/logo.png" style="width:100%">
+    </div>
+
+    <div class="mySlides2">
+        <img src="images/appstore.png" style="width:100%">
+    </div>
+
+    <div class="mySlides2">
+        <img src="images/tilbud.png" style="width:100%">
+    </div>
+
+    <a class="prev" onclick="plusSlides(-1, 1)">&#10094;</a>
+    <a class="next" onclick="plusSlides(1, 1)">&#10095;</a>
+</div>
+
+<script>
+    var slideIndex = [1,1];
+    var slideId = ["mySlides1", "mySlides2"]
+    showSlides(1, 0);
+    showSlides(1, 1);
+
+    function plusSlides(n, no) {
+        showSlides(slideIndex[no] += n, no);
+    }
+
+    function showSlides(n, no) {
+        var i;
+        var x = document.getElementsByClassName(slideId[no]);
+        if (n > x.length) {slideIndex[no] = 1}
+        if (n < 1) {slideIndex[no] = x.length}
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        x[slideIndex[no]-1].style.display = "block";
+    }
+</script>
 
 <?php include "includes/footer.php"; ?>
 
