c71365570c29e591dcf6c18a03f213d05bb7739b
Marie <Mari400m@edu.zealand.dk>
2022-03-14 15:35:45 +0100

    slideshow tid

--- index.php
+++ index.php
@@ -32,14 +32,14 @@
     <div class="col-6">
         <div id="carousel1" class="carousel slide" data-bs-ride="carousel">
             <div class="carousel-inner">
-                <div class="carousel-item active" data-bs-interval="10000">
+                <div class="carousel-item active" data-bs-interval="5000">
                     <img src="images/1.png" class="d-block w-100" alt="...">
                 </div>
-                <div class="carousel-item" data-bs-interval="2000">
-                    <img src="images/2.png" class="d-block w-100" alt="...">
+                <div class="carousel-item" data-bs-interval="1000">
+                    <img src="images/logo.png" class="d-block w-100" alt="...">
                 </div>
                 <div class="carousel-item">
-                    <img src="images/logo.png" class="d-block w-100" alt="...">
+                    <img src="images/2.png" class="d-block w-100" alt="...">
                 </div>
             </div>
             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
@@ -56,14 +56,14 @@
     <div class="col-6">
         <div id="carousel2" class="carousel slide" data-bs-ride="carousel">
             <div class="carousel-inner">
-                <div class="carousel-item active" data-bs-interval="10000">
-                    <img src="images/1.png" class="d-block w-100" alt="...">
+                <div class="carousel-item active" data-bs-interval="5000">
+                    <img src="images/logo.png" class="d-block w-100" alt="...">
                 </div>
-                <div class="carousel-item" data-bs-interval="2000">
+                <div class="carousel-item" data-bs-interval="1000">
                     <img src="images/2.png" class="d-block w-100" alt="...">
                 </div>
                 <div class="carousel-item">
-                    <img src="images/logo.png" class="d-block w-100" alt="...">
+                    <img src="images/1.png" class="d-block w-100" alt="...">
                 </div>
             </div>
             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
