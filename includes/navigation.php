6da6bb79ec2ed36d478118849fc13a2ecb02680e
Oline <olinep@live.dk>
2022-03-16 11:20:47 +0100

    lavet menu om til link til forsiden index

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,5 +1,5 @@
 <div class="topnav d-flex justify-content-evenly">
-    <a class="active" href="#menu">Menu</a>
+    <a class="active" href="index.php">Menu</a>
     <a href="#Butikker">Butikker</a>
     <img src="images/logo.png" style="max-width: 7%" alt="logo top">
     <a href="kontakt.php">Kontakt</a>
