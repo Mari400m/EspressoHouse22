1ec3f6d527c3e0fa90e7f9190d823ce03eb37f49
Marie <Mari400m@edu.zealand.dk>
2022-03-16 12:42:27 +0100

    carussel

--- index.php
+++ index.php
@@ -22,7 +22,6 @@
 <?php include "includes/navigation.php"; ?>
 
 <div class="row bg-kortbaggrund">
-    <div class="col-6">
         <div id="carousel1" class="carousel slide" data-bs-ride="carousel">
             <div class="carousel-inner">
                 <div class="carousel-item active" data-bs-interval="2000">
@@ -46,32 +45,6 @@
                 <span class="visually-hidden">Next</span>
             </button>
         </div>
-    </div>
-    <div class="col-6">
-        <div id="carousel2" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item active" data-bs-interval="2000">
-                    <img src="images/logo.png" class="d-block w-100" alt="...">
-                </div>
-                <div class="carousel-item" data-bs-interval="500">
-                    <img src="images/2.png" class="d-block w-100" alt="...">
-                </div>
-                <div class="carousel-item">
-                    <img src="images/1.png" class="d-block w-100" alt="...">
-                </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval"
-                    data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval"
-                    data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
-        </div>
-    </div>
 </div>
 
 <br class="text-break bg-kortbaggrund">
@@ -100,6 +73,7 @@
         <div class="col-md-8">
             <div class="card-body">
                 <h5 class="card-title">TOMORROW FRIENDLY</h5>
+                <br>
                 <p class="card-text">
                     At Espresso House we take sustainability seriously and like many,
                 </p>
@@ -118,6 +92,7 @@
                 <p>
                     just that.
                 </p>
+                <br><br>
                 <p class="card-text"><small class="text-muted">Read more at www.tomorrowfriendly.com</small></p>
             </div>
         </div>
