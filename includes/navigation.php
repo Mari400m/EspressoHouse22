e11f4189853c34c6498fbe3860443afdb4552f81
Marie <Mari400m@edu.zealand.dk>
2022-03-17 16:40:16 +0100

    lavet nogle underside rettelser

--- includes/navigation.php
+++ includes/navigation.php
@@ -3,5 +3,6 @@
     <a href="butikker.php">Butikker</a>
     <img src="images/logo.png" style="max-width: 7%" alt="logo top">
     <a href="kontakt.php">Kontakt</a>
-    <a href="omos.php">Om os</a>
+    <a href="../voreskaffe.php">Vores kaffe</a>
+
 </div>
---
+++ voreskaffe.php
@@ -0,0 +1,316 @@
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title> Vores kaffe </title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <script src="https://kit.fontawesome.com/e107c235f0.js" crossorigin="anonymous"></script>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+</head>
+
+<body>
+
+<?php include "includes/navigation.php"; ?>
+
+<br>
+<br>
+
+<h1>Vores kaffe</h1>
+<br>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col">
+            <p>På Espresso House brænder vi for en fantastisk kaffeoplevelse.</p>
+            <p>Alt vores kaffe er specialkaffe, eller Specialty coffee, som det hedder på engelsk.
+            <p>
+            <p>Det betyder, at kaffen altid når over 80 points på en kvalitetsskala fra 0–100.</p>
+            <p>Det er der kun 5 % af alt kaffe i verden, der gør.
+            <p>
+        </div>
+        <div class="col">
+            <p>Med andre ord kan vi bogstaveligt talt sige, at vores kaffe er i verdensklasse.</p>
+            <p>Friskheden er desuden vigtig for, at kaffen kan udfolde sin fulde smagskarakter.</p>
+            <p>Friskristet kaffe har flest delikate smagsnuancer og aromaer, og på vores helt eget risteri, uden for Stockholm,</p>
+            <p>rister vi vores kaffe langsomt og varsomt, så hver bønnes unikke karakter opnår sit fulde potentiale.</p>
+            <p>For at sikre friskheden på vores kaffe, markerer vi alle poser med ristningsdato- og tidspunkt.</p>
+        </div>
+    </div>
+</div>
+
+
+
+<br>
+<br>
+
+<div id="accordion">
+
+    <div class="card">
+        <div class="card-header">
+            <a class="collapsed btn" data-bs-toggle="collapse" href="#collapseOne">
+                <h3>Kaffe derhjemme</h3>
+            </a>
+        </div>
+        <div id="collapseOne" class="collapse" data-bs-parent="#accordion">
+            <div class="card-body">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col">
+                            <br>
+                            <p>Hos Espresso House er kaffe hjertet i alt vi gør. I alle vores kaffebarer ønsker vi at
+                                skabe en pause og en tid til hygge og samvær.</p>
+                            <p>Og det samme ønsker vi, at du skal kunne gøre derhjemme.</p>
+                            <br>
+                            <h5>Nyd din Espresso House kvalitetskaffe derhjemme.</h5>
+                            <br>
+                            <p>Uanset om du ønsker at bruge hele bønner, filterkaffe eller kapsler, har vi noget der
+                                passer din smag.</p>
+                            <br>
+                            <h5>Forhandlere:</h5>
+                            <br>
+                            <p>Espresso House kaffe forhandles i Meny og udvalgte dagligvarebutikker bl.a. hos Kvickly,
+                                SuperBrugsen, Bilka og Føtex.</p>
+                            <br>
+                        </div>
+                        <div class="col">
+                            <br>
+                            <h5>Tips til kaffebrygning i hjemmet</h5>
+
+                            <p>Vi har nedskrevet nogle tips til, når du skal være din egen barista derhjemme:</p>
+
+                            <p>1. Køb altid hele bønner og kværn kun det du skal bruge, hvis du har mulighed for det!</p>
+                            <p>2. Skulle du købe færdigmalet kaffe, anbefaler vi, at den opbevares i en lufttæt beholder, for
+                            at bevare så meget smag som muligt!</p>
+                            <p>3. Når du skal brygge kaffe, så skyld altid dit kaffefilter inden du bruger det, for at fjerne
+                                eventuelle smagspåvirkninger det måtte sidde i filteret</p>
+                            <p>4. En god tommelfingerregel til brygning af kaffe er, at der bruges 6 gram kaffe til 100 gram
+                                vand</p>
+                            <p>5. Hvis muligt, så brug vand uden kalk, da kalk neutraliserer nogle af de fine og syrlige smage
+                                i kaffen</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-header">
+            <a class="collapsed btn" data-bs-toggle="collapse" href="#collapseTwo">
+                <h3>Vores risteri</h3>
+            </a>
+        </div>
+        <div id="collapseTwo" class="collapse" data-bs-parent="#accordion">
+            <div class="card-body">
+                <br>
+                <p>I Länna syd for Stockholm har vi længe brygget på noget stort.</p>
+                <p>Her har vi nemlig bygget vores helt eget kafferisteri, og det er ikke bare hvilket som helst risteri.</p>
+                <p>Det er Europas største og mest moderne specialkafferisteri!</p>
+                <p>Nu rister vi altså selv alt vores kaffe til alle vores coffee shops.</p>
+                <br>
+            </div>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-header">
+            <a class="collapsed btn" data-bs-toggle="collapse" href="#collapseThree">
+                <h3>Vores filterkaffe</h3>
+            </a>
+        </div>
+        <div id="collapseThree" class="collapse" data-bs-parent="#accordion">
+            <div class="card-body">
+                <br>
+                <p>Hvad skulle det være? En kop af verdens bedste kaffe? Eller vores fineste sæsonkaffe?</p>
+                <br>
+                <p>Det er faktisk det, vi spørger om, når vores Baristaer tilbyder dig vores Signature Brew eller Seasonal Brew.</p>
+                <p>Vi brygger nemlig altid to forskellige varianter af specialkaffe i vores coffee shops, så du kan finde din personlige favorit.</p>
+                <br>
+                <h5>Signature Brew</h5>
+                <br>
+                <p>Vores klassiske specialkaffe kommer fra fire udvalgte kaffefarmere i Cerrado-området i Brasilien.</p>
+                <p>Det bliver fragtet til vores eget risteri i Länna uden for Stockholm,</p>
+                <p>hvor vi langsomt og varsomt rister bønnerne til perfektion.</p>
+
+                <p>Du vil smage en mellemristet, fyldig og rund specialkaffe med en lang eftersmag og tydelige noter af chokolade, nødder og marcipan.</p>
+                <p>Et kram i en kaffekop.</p>
+
+                <br>
+                <p>Fire gange om året præsenterer vi derfor en særligt udvalgt specialkaffe, som du kan forkæle dine smagsløg ekstra med.</p>
+                <p>Vi kalder vores limited edition for Seasonal Brew, og den er lidt finere end verdens bedste kaffe.</p>
+                <br>
+            </div>
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            <a class="collapsed btn" data-bs-toggle="collapse" href="#collapseFour">
+                <h3>Vores Espresso</h3>
+            </a>
+        </div>
+        <div id="collapseFour" class="collapse" data-bs-parent="#accordion">
+            <div class="card-body">
+                <br>
+
+                <p>Espressoen er hjertet i vores forretning og kernen i mange af vores drikke.</p>
+                <p>Vi har søgt, sorteret og smagt utallige kaffer for at sammensætte den helt perfekte blend til dig.</p>
+
+                <br>
+
+                <p>Den består i dag af bønner fra Brasilien, Colombia og Burundi, og alt efter sæsonen skifter bønnesammensætningen for at kaffen altid er så frisk som mulig.</p>
+                <p>Vores Roastmaster sørger for, at risteprocessen er helt optimal for netop vores kaffe, så vi fremkalder den rige sammensætning af smagsnuancer, den indeholder.</p>
+                <p>Resultatet er vores Signature Espresso, som er fyldig og karakterfuld med sødme, friskhed og noter af chokolade, karamel og appelsin.</p>
+                <p>Smagen er perfekt afbalanceret, hvad enten du nyder din espresso ren eller med dampet, varm mælk.</p>
+
+                <br>
+
+                <p>Vores baristaer håndbrygger altid din drik personligt til dig.</p>
+                <p>Specialønsker som ”ekstra varm”, ”mindre mælk”, ”ekstra kraftig” og ”mere skum” er sød musik i vores ører.</p>
+                <p>Vi vil nemlig altid give verdens bedste kaffeoplevelse til verdens bedste gæster.</p>
+
+                <br>
+            </div>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-header">
+            <a class="collapsed btn" data-bs-toggle="collapse" href="#collapseFive">
+                <h3>Vores kaffebønder</h3>
+            </a>
+        </div>
+        <div id="collapseFive" class="collapse" data-bs-parent="#accordion">
+            <div class="card-body">
+                <br>
+                <p>Hos Espresso House ønsker vi at tage ansvar fra bønne til kop for at sikre, at vi er en del af en bæredygtig kaffeproduktion.</p>
+                <br>
+                <h5>Bæredygtigt indkøb</h5>
+                <br>
+                <p>Ud over vores adfærdskodeks for leverandører vejledes vi af vores interne program når kaffebønnerne skal indkøbes.</p>
+                <p>Den indeholder rutiner, tiltag og principper, som vi følger for at sikre et bæredygtigt indkøb.</p>
+                <p>Programmet inkluderer interne besøg og vurderinger af vore kaffebønder samt eksterne tredjepartsvurderinger foretaget af landmænd og samarbejdspartnere.</p>
+                <p>Arbejdet koordineres i et internt råd med medlemmer fra både Espresso House og Johan & Nyström.</p>
+                <br>
+            </div>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-header">
+            <a class="collapsed btn" data-bs-toggle="collapse" href="#collapseSix">
+                <h3>Vores kaffeskole</h3>
+            </a>
+        </div>
+        <div id="collapseSix" class="collapse" data-bs-parent="#accordion">
+            <div class="card-body">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col">
+                            <br>
+                            <h5>Tips fra Baristaen</h5>
+                            <br>
+                            <p>Der er meget, der skal gå op i en højere enhed for at koppen foran dig</p>
+                            <p>smager lige så fantastisk, som kun kaffe kan.</p>
+                            <br>
+                            <p>Først dyrkningen, plukningen og bearbejdningen af kaffebærret,</p>
+                            <p>dernæst fragten af bønnerne fra den ene ende af jordkloden til den anden,</p>
+                            <p>og så den helt korrekte ristning, før din kaffe endelig kan brygges, serveres og nydes.</p>
+                            <br>
+                            <p>En lang rejse for kaffebønnen, som er forgæves, hvis vi ikke tager hånd om den og brygger den på den rigtige måde.</p>
+                            <br>
+                            <p>Derfor har vi samlet nogle konkrete tips til, hvordan du kan brygge din gode kaffe derhjemme</p>
+                            <p>og lykkes med denne sidste, men nok så vigtige, etape af kaffens bearbejdning.</p>
+                            <br>
+                        </div>
+                        <div class="col">
+                            <br>
+                            <h5>Bønner</h5>
+                            <br>
+                            <p>- Køb hele bønner og mal dem selv- færdigmalet kaffe taber hurtigt smag</p>
+                            <br>
+                            <p>- Investér i en ordentligt kværn til hjemmebrug
+                                (f.eks. en Wilfa, som du kan købe hos os): mal altid bønnerne, lige inden du brygger kaffen</p>
+                            <br>
+                            <p>- Mal aldrig mere, end du behøver fra gang til gang</p>
+                            <br>
+                            <p>- Opbevar altid bønnerne i deres originale emballage, godt tilsluttet (med en klemme)
+                                – sørg for at der er så lidt luft som muligt i posen</p>
+                            <br>
+                            <p>- Opbevar kaffen i stuetemperatur (det er en myte, at den skal opbevares i fryseren)</p>
+                            <br>
+                            <p>- Kaffe er en friskvare: Køb hellere en lille pose med hele bønner end en stor, hvis dit forbrug er begrænset</p>
+                            <br>
+                            <p>- Tag dig mod til at prøve noget nyt: der findes mange forskellige alternativer i såvel dagligvarehandlen som i specialbutikker</p>
+                            <br>
+                            <p>- Kast dig ud i at prøve noget andet end mørkristet kaffe:
+                                Smagen bliver ikke bedre af at bønnerne ristes for hårdt.
+                                På den måde forsvinder nemlig mange subtile smagsnuancer</p>
+                            <br>
+                            <p>- Overdosér ikke mængden af kaffe: Du risikerer, at kaffen kommer til at smage bittert</p>
+                        <br>
+                        </div>
+                        <div class="col">
+                            <br>
+                            <h5>Tilberedning</h5>
+                            <br>
+                            <p>- Steam kold mælk: den bliver bedre sådan</p>
+                            <br>
+                            <p>- Når du steamer mælk: Sørg for, at tuden på steamerarmen er lige på
+                                overfladen, når du begynder, for derefter langsomt at nedsænke den
+                                i mælken, som nu bliver varm. Hvis det “hyler” i røret, gør du det
+                                forkert</p>
+                            <br>
+                            <p>- Tør altid steamerarmen af, lige efter du har brugt den – ellers bliver
+                                den svær at få ren igen, da mælken brænder sig fast</p>
+                            <br>
+                            <p>- Husk på, at mælkebaserede kaffedrikke er en friskvare: når du holder
+                                dem varme i en termokop eller lignende, kommer de ikke til deres ret.
+                                Nyd drikken med det samme!</p>
+                            <br>
+                            <p>- Køb en porcelænsfilterholder og bryg kaffen direkte i koppen:
+                                hæld friskmalet kaffe i filtret, fugt kaffen med friskkogt vand.
+                                Lad det afkøle lidt.
+                                Tilsæt til sidst resten af vandet jævnt og langsomt.
+                                Utroligt godt.
+                                Eller køb en Chemexbrygger (sælges i vores coffee shops) og bryg din kaffe som en rigtig professionel</p>
+                            <br>
+                            <p>- Hold ikke filterkaffe varm.
+                                Gør som i vores coffee shops: opbevar den friskbryggede kaffe i en termokande af god kvalitet.
+                                Hvis kaffen varmes for meget og for længe, bliver den bitter</p>
+                            <br>
+                        </div>
+                        <div class="col">
+                            <br>
+                            <h5>Andre drikke</h5>
+                            <br>
+                            <p>- Anvend ikke kogende vand til grøn te.
+                                Den skal brygges med koldere vand.
+                                Hæld en anelse kold vand i det varme, eller læg en lille isklump i</p>
+                            <br>
+                            <p>- For at opnå samme konsistens på din chai hjemme, er det bedst at steame mælken eller
+                                – hvis du ikke har mulighed for at steame mælken – at piske den op, mens den varmes.
+                                Bland det derefter med chaipulveret (som kan købes i vores coffee shops)</p>
+                            <br>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+<br><br>
+
+<?php include "includes/footer.php"; ?>
+
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
