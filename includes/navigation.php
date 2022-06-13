0d8cd994c94de0587c942d3115312a3777be6517
Oline <olinep@live.dk>
2022-06-13 11:08:38 +0200

    rettet billed højde så det passer bedre med hvordan brugeren ser det

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,7 +1,6 @@
-<div class="topnav d-flex justify-content-evenly">
+<div class="topnav d-flex justify-content-evenly position-relative">
     <a class="active" href="index.php">Menu</a>
-    <a href="butikker.php">Butikker</a>
-    <img src="images/logo.png" style="max-width: 7%" alt="logo top">
+    <a href="butikker.php">Butikker</a><img src="images/logo.png" style="max-width: 150px;" alt="logo top">
     <a href="kontakt.php">Kontakt</a>
     <a href="voreskaffe.php">Vores kaffe</a>
 
